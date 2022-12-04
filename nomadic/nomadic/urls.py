4a37e5300b71c7890b5aa95af5a4069413153aae
sriharshanaladala <83389426+sriharshanaladala@users.noreply.github.com>
2022-12-04 07:43:53 +0530

    Merge remote-tracking branch 'origin/master'

--- nomadic/nomadic/urls.py
+++ nomadic/nomadic/urls.py
@@ -1,18 +1,9 @@
 
 from django.contrib import admin
-from django.urls import path
-from django.http import HttpResponse
-
-
-def places(request):
-    return HttpResponse("here are our best place to visit")
-
-def place(request, pk):
-    return HttpResponse("single place"+ '' + str(pk))
+from django.urls import path, include
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("places/", places, name="places"),
-    path('place/<str:pk>', place, name="place"),
+    path('', include('places.urls'))
 ]
--- nomadic/places/urls.py
+++ nomadic/places/urls.py
@@ -1,1 +1,8 @@
+from django.urls import path
+from . import views
 
+
+urlpatterns = [
+    path("places/", views.places, name="places"),
+    path('place/<str:pk>', views.place, name="place"),
+]
